710a39cba602ca0028a9e556240ccdb0978e4e28
PetricorZ <1584416275@qq.com>
2024-12-25 18:19:24 +0800

    上传图片

--- src/main/java/org/lanqiao/util/PictureUtil.java
+++ src/main/java/org/lanqiao/util/PictureUtil.java
@@ -1,39 +1,48 @@
 package org.lanqiao.util;
 
-import jakarta.servlet.ServletOutputStream;
+
+import java.io.*;
+
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Part;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
+@MultipartConfig
 public class PictureUtil {
-    public static void respImg(String imgPath, HttpServletResponse resp) {
-        try {
-            //1.把图片数据读进来
-            FileInputStream is = new FileInputStream(imgPath);
 
-            ServletOutputStream os = resp.getOutputStream();
 
-            //构建缓冲流，提高读写效率
-            BufferedInputStream bis = new BufferedInputStream(is);
-            BufferedOutputStream bos = new BufferedOutputStream(os);
-            byte[] bytes = new byte[1000];
+    public static void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 获取上传的文件
+        Part filePart = request.getPart("image");
+        String fileName = filePart.getSubmittedFileName();
 
-            //2.循环，边读边写
-            while (bis.read(bytes) != -1) {
-                bos.write(bytes);
-            }
+        // 设置存储路径
+        String savePath = "D:/1/" + fileName;
 
-            //3.关闭资源
-            bos.flush();
-            bos.close();
-            bis.close();
-            is.close();
-            os.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        // 获取输入流
+        InputStream fileContent = filePart.getInputStream();
+
+        // 创建输出流
+        OutputStream out = Files.newOutputStream(new File(savePath).toPath());
+
+        // 将文件内容写入输出流
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = fileContent.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
         }
 
+        // 关闭流
+        fileContent.close();
+        out.close();
+
+        response.getWriter().write("文件上传成功！");
     }
 }
