17c82cf60af7a2b59f91ae870f4b04ad4c1b4031
hmg <3093631482@qq.com>
2024-12-30 19:06:45 +0800

    链接

--- src/main/webapp/MainModule/Mainjs/store.js
+++ src/main/webapp/MainModule/Mainjs/store.js
@@ -3,5 +3,17 @@ $(document).ready(()=>{
             var index = $(this).attr('data-index');
             sessionStorage.setItem('index',index);
             window.open('vr.html');
-    });
+        });
+        $(document).ready(function () {
+            $('.bar_content').click(function () {
+                    console.log($(this).index());
+                    if($(this).index()===0){
+                        window.location.href = 'Main.html';
+                    }else if($(this).index()===1){
+                        window.location.href = 'storehouse.html';
+                    }else if($(this).index()===4){
+                        window.location.href = 'message.html';
+                    }
+                });
+        });
 })
