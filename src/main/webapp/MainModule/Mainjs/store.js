5d77ee9fd0f18c37bd6d5da9de5b12c776a5e047
Ocean105Hu <1656067205@qq.com>
2025-01-02 10:04:07 +0800

    哈哈哈

--- src/main/webapp/MainModule/Mainjs/store.js
+++ src/main/webapp/MainModule/Mainjs/store.js
@@ -1,19 +1,25 @@
-$(document).ready(()=>{
-        $('.lattice').click(function(){
-            var index = $(this).attr('data-index');
-            sessionStorage.setItem('index',index);
-            window.open('vr.html');
+$(document).ready(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get('username');
+    if (username != null) {
+        $('.nav_login a').text(username);
+    }
+    $('.lattice').click(function () {
+        var index = $(this).attr('data-index');
+        sessionStorage.setItem('index', index);
+        window.open('vr.html');
+    });
+    $(document).ready(function () {
+        $('.bar_content').click(function () {
+            console.log($(this).index());
+            if ($(this).index() === 0) {
+                window.location.href = `Main.html?username=${encodeURIComponent(username)}`;
+            } else if ($(this).index() === 1) {
+                window.location.href = 'storehouse.html';
+            } else if ($(this).index() === 4) {
+                window.location.href = 'message.html';
+            }
         });
-        $(document).ready(function () {
-            $('.bar_content').click(function () {
-                    console.log($(this).index());
-                    if($(this).index()===0){
-                        window.location.href = 'Main.html';
-                    }else if($(this).index()===1){
-                        window.location.href = 'storehouse.html';
-                    }else if($(this).index()===4){
-                        window.location.href = 'message.html';
-                    }
-                });
-        });
+    });
+
 })
