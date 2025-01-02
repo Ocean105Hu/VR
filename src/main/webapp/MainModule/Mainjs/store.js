61ded6fc75777d89d4de7f4bc2a1c0a20ac7cf3e
hmg <3093631482@qq.com>
2025-01-02 11:49:26 +0800

    上传图片修改

--- src/main/webapp/MainModule/Mainjs/store.js
+++ src/main/webapp/MainModule/Mainjs/store.js
@@ -9,17 +9,49 @@ $(document).ready(() => {
         sessionStorage.setItem('index', index);
         window.open('vr.html');
     });
-    $(document).ready(function () {
-        $('.bar_content').click(function () {
-            console.log($(this).index());
-            if ($(this).index() === 0) {
-                window.location.href = `Main.html?username=${encodeURIComponent(username)}`;
-            } else if ($(this).index() === 1) {
-                window.location.href = 'storehouse.html';
-            } else if ($(this).index() === 4) {
-                window.location.href = 'message.html';
-            }
-        });
+    $('.bar_content').click(function () {
+        console.log($(this).index());
+        if ($(this).index() === 0) {
+            window.location.href = `Main.html?username=${encodeURIComponent(username)}`;
+        } else if ($(this).index() === 1) {
+            window.location.href = 'storehouse.html';
+        } else if ($(this).index() === 4) {
+            window.location.href = 'message.html';
+        }
     });
+    const arr = [
+        {text:"台湾",author:"中国台湾"},
+        {text:"北京-八达岭",author:"爱吃哈密瓜"}
+    ]
+    let preindex = ($('.lattice').last().index())+1;
+    console.log(preindex);
+    let insertdiv =(inedx,text,author)=>`<div class="lattice" data-index="1-${index}">
+                <a class="card" target="_blank">
+                    <div class="wrapper-img">
+                        <img class="changeImgSize" src="./img/vr_img/1-${index}/thumb.jpg" alt="马来西亚-吉隆坡VR全景">
+                    </div>
+                    <div class="lattice-name">${text}</div>
+                    <div class="card-actions">
+                        <div class="pull-left">
+                            <img style="width: 28px; height:28px; overflow:hidden; " src="img/vr_img/icon.png" alt="">
+                            <span class="rephoto-author">${author}</span>
+                        </div>
+                        <div class="pull-right">
+                            <img src="img/vr_img/see.png" style="width: 18px;height: 12px;display: inline-block;margin-bottom: 3px;margin-right:5px" alt="">
+                            72457
+                        </div>
+                    </div>
+                </a>
+            </div>`;
+    let imagePath = `img/vr_img/1-${preindex}/pano_b.jpg`;
+
+    let img = new Image();
+    img.src = imagePath;
+
+    img.onload = function() {
+        arr.forEach((i)=>{
+            $('pictures_content').append(insertdiv(preindex,i.text,i.author))
+        });
+    };
 
 })
